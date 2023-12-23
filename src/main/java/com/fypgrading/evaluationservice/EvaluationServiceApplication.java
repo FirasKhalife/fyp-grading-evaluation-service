43d052df54a4b7e8c68391284a2973d76575bcbd
Gaelle327 <gaellesaid65@gmail.com>
2023-12-23 17:25:09 +0200

    adding aop for evaluation pipeline

--- src/main/java/com/fypgrading/evaluationservice/EvaluationServiceApplication.java
+++ src/main/java/com/fypgrading/evaluationservice/EvaluationServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableMongoRepositories("com.fypgrading.evaluationservice.repository")
+@EnableAspectJAutoProxy
 public class EvaluationServiceApplication {
 
 	public static void main(String[] args) {
