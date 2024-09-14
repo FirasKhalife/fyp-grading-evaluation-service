e4e51dd4d28e8e0917d77abf66b20dce0f324774
Firas Khalife <khalifefiras29@gmail.com>
2024-09-14 21:34:33 +0300

    cloud-course improve configuration and env vars

--- src/main/java/com/fypgrading/evaluationservice/config/MongoConfig.java
+++ src/main/java/com/fypgrading/evaluationservice/config/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -17,11 +18,13 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Value("${spring.data.mongodb.uri}")
     private String uri;
 
+    @NotNull
     @Override
     protected String getDatabaseName() {
         return "evaluation-db";
     }
 
+    @NotNull
     @Override
     public MongoClient mongoClient() {
         ConnectionString connectionString = new ConnectionString(uri);
@@ -32,8 +35,9 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         return MongoClients.create(mongoClientSettings);
     }
 
+    @NotNull
     @Override
-    public Collection getMappingBasePackages() {
+    public Collection<String> getMappingBasePackages() {
         return Collections.singleton("com.fypgrading.reviewservice.entity");
     }
 }
